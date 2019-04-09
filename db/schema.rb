e37e08f3bcd40107863ee30522085c025317936f
HENRY ANDERSON AOVARE <alg70021@gmail.com>
2019-04-09 04:28:04 -0800

    Styled form buttons and setup admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_02_213251) do
+ActiveRecord::Schema.define(version: 2019_04_09_121459) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2019_04_02_213251) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "stripe_id"
+    t.string "card_brand"
+    t.string "card_last4"
+    t.string "card_exp_month"
+    t.string "card_exp_year"
+    t.datetime "expires_at"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
