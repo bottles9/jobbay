79b42a4ebc6f32bb99b188f728bd9d6948771b6e
HENRY ANDERSON AOVARE <alg70021@gmail.com>
2019-04-10 16:23:04 -0800

    Required turbolinks in javascript and styled the index view

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,10 @@ class Job < ApplicationRecord
 	belongs_to :user
 	mount_uploader :avatar, AvatarUploader
 
+	validates_presence_of  :title, :apply_url, :description, :url,
+      :job_type,
+      :location
+     
+
 	JOB_TYPES = ["Full-time", "Part-time", "Contract", "Freelance"]
 end
