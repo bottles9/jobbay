9c98b0d4c2fdbb9a209750edcb136d4ca4ce9859
HENRY ANDERSON AOVARE <alg70021@gmail.com>
2019-04-12 17:11:38 -0800

    Basic styles

---
+++ app/assets/javascripts/card.js
@@ -0,0 +1,13 @@
+
+new Card({
+  form: 'form',
+  container: '.card',
+  formSelectors: {
+    numberInput: 'input[name=number]',
+    expiryInput: 'input[name=expiry]',
+    cvcInput: 'input[name=cvv]',
+    nameInput: 'input[name=name]'
+  },
+  width: 390, // optional — default 350px
+  formatting: true
+})
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,11 +1,6 @@
 class Job < ApplicationRecord
 	belongs_to :user
-	mount_uploader :avatar, AvatarUploader
-
-	validates_presence_of  :title, :apply_url, :description, :url,
-      :job_type,
-      :location
-     
+	mount_uploader :avatar, AvatarUploader     
 
 	JOB_TYPES = ["Full-time", "Part-time", "Contract", "Freelance"]
 end
